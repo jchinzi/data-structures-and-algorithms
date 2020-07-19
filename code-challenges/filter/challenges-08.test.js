ce5c6aadeb624fd34d86b585fbcd878b015b0814
Jennifer Chinzi <jenrose87@gmail.com>
2020-07-19 10:12:54 -0700

    All Challenges Passing

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -187,7 +187,13 @@ For example: evenOddNumericValues(['Gregor', 2, 4, 1]) returns ['even', 'even', 
 ------------------------------------------------------------------------------------------------ */
 
 const evenOddNumericValues = (arr) => {
-  // Solution code here...
+  let allNumArr = arr.filter(each => typeof each === 'number');
+  let wordVersion = allNumArr.map(num => {
+    if (num % 2 === 0){
+      return 'even';
+    } else return 'odd';
+  })
+  return wordVersion;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -312,7 +318,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should remove non-integers and return "even" or "odd', () => {
     expect(evenOddNumericValues(['Gregor', 2, 4, 1])).toStrictEqual(['even', 'even', 'odd']);
     expect(evenOddNumericValues(['Gregor', 2, 4, 1]).length).toStrictEqual(3);
